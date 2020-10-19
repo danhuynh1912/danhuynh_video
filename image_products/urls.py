df2c8cede49b85b331f07042ee95cc309c5c389c
danhuynh1912 <danhuynh1912@gmail.com>
2020-10-19 17:45:51 +0700

    aloalo

--- image_products/urls.py
+++ image_products/urls.py
@@ -4,5 +4,6 @@ from . import views as products_view
 app_name = 'products'
 
 urlpatterns = [
-	path('', products_view.ProductsView.as_view(), name='products')
+	path('', products_view.ProductsView.as_view(), name='products'),
+	path('information-product/<int:thisId>', products_view.ProductInforView.as_view(), name='productsInfor'),
 ]
--- image_products/views.py
+++ image_products/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django import views
+from . import models as images_models
 # Create your views here.
 
 class ProductsView(views.View):
 	def get(self, request):
-		return render(request, 'products/index.html')
+		imgs = images_models.ImagesProducts.objects.all()
+		context = {'imgs': imgs}
+		return render(request, 'products/index.html', context)
+
+
+class ProductInforView(views.View):
+	def get(self, request, thisId):
+		infor = images_models.ImagesProducts.objects.get(pk = thisId)
+		products = images_models.ImagesProducts.objects.exclude(id = infor.id)
+		context = {'infor': infor, 'products': products}
+		print(type(products))
+		return render(request, 'products/detailthisproduct.html', context)
